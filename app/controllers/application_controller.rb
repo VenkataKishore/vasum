f930440689112e921d5401bd2f9ec6cb96c26ac0
bre2-PC <bre-2@bre2-PC>
2015-03-06 10:27:54 +0530

    assets precompile fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  USER_NAME, PASSWORD = "vasum", "vasum"
+
+  before_filter :authenticate
+
+  private
+  def authenticate
+    authenticate_or_request_with_http_basic do |user_name, password|
+      user_name == USER_NAME && password == PASSWORD
+    end
+  end
+
 end
